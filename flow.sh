6f1d4507849b1509ff69eff5b013aaf17d52d521
yeomdaehun <kr.dev.hoon@gmail.com>
2023-02-19 15:31:06 +0900

    머지용 스크립트 생성
    - 머지시 스크립트를 통해 특정 브랜치들 재생성

--- flow.sh
+++ flow.sh
@@ -1,13 +1,62 @@
 #!/bin/bash
 
-# git 명령어 실행 후 결과를 변수에 저장
-output=$(git status)
+BRANCH="release-release"
 
-# 결과를 파싱
-if [[ $output == *"Changes not staged for commit"* ]]; then
-  echo "변경사항이 스테이지에 추가되지 않았습니다."
-elif [[ $output == *"Changes to be committed"* ]]; then
-  echo "변경사항이 스테이지에 추가되었습니다."
-else
-  echo "변경사항이 없습니다."
-fi
+new_branches=(
+  qa
+  test
+  release-release
+)
+
+managed_braches=(
+  master
+  dev
+  test
+  qa
+  release-release
+)
+
+DATE=$(date +%Y.%m.%d)
+
+git checkout $BRANCH
+
+git pull origin $BRANCH
+
+tempfile=$(mktemp)
+
+echo "#Enter Commit Feature Message ( ex. - feature (BE-XXXX) : ... )" >"$tempfile"
+
+vim "$tempfile"
+commit_message=$(grep -v '^#' "$tempfile")
+
+echo "$commit_message" >"$tempfile"
+rm -f "$tempfile"
+
+master_msg="Merge branch '$BRANCH'
+
+$commit_message"
+
+git checkout master
+
+git merge --no-ff $BRANCH -m "$master_msg"
+
+git checkout dev
+
+dev_msg="feature (배포) $DATE
+
+$commit_message"
+
+git merge --no-ff master -m "$dev_msg"
+
+for new_branch in "${new_branches[@]}"; do
+  git branch -D "$new_branch"
+  git branch "$new_branch" dev
+done
+
+for push_branch in "${managed_braches[@]}"; do
+  git push -u -f origin "$push_branch"
+done
+
+git push -f origin "$push_branch"
+
+echo "Success"
